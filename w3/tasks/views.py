63906985be33830b499e5866ca2f15adc334d8a5
Michael Hofweller <michael.hofweller@gmail.com>
2021-01-28 13:45:22 -0800

    finished django

--- w3/tasks/views.py
+++ w3/tasks/views.py
@@ -1,14 +1,38 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz"]
+
 # Create your views here.
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task", max_length=100)
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
 
 
 def index(request):
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": request.session["tasks"]
     })
 
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        # process the result of the request
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
