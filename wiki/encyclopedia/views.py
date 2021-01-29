b26949cd4db4d5bc7b2e828a18564d0ff2a6b316
Michael Hofweller <michael.hofweller@gmail.com>
2021-01-29 10:27:11 -0800

    wiki

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
 from . import util
+from django.http import HttpResponse
 
 
 def index(request):
@@ -8,3 +8,10 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def getpage(request, title):
+    page = util.get_entry(title)
+    # if page:
+    return render(request, page)
+    # else:
+    # return HttpResponse('<h1>404 Page was found</h1>')
