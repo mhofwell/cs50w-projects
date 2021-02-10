40239b8102558b46425e99c267deb3b7245e725a
Michael Hofweller <michael.hofweller@gmail.com>
2021-02-09 16:36:19 -0800

    modelform functionality complete

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,19 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from datetime import datetime, date
+from django.forms import ModelForm
+from django import forms
+from django.forms.models import BaseInlineFormSet
+from django.forms.widgets import NumberInput, TextInput, Textarea
 
+
+# Models
 
 class User(AbstractUser):
     pass
 
 
 class AuctionListing(models.Model):
-    listing_title = models.CharField(max_length=100, default="None")
-    description = models.TextField(max_length=500, default="None")
-    category = models.CharField(max_length=25, default="None")
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    current_bid = models.DecimalField(
-        max_digits=6, decimal_places=2, default=0.00)
+    listing_title = models.CharField(max_length=100)
+    description = models.TextField(
+        max_length=500)
+    category = models.CharField(max_length=25)
+    starting_bid = models.FloatField(default=0.00)
+    current_bid = models.FloatField(default=0.00)
     image = models.ImageField(upload_to='auctions/uploads/')
     date_created = models.DateField(auto_now_add=True)
 
@@ -25,8 +31,35 @@ class Bid(models.Model):
 class Comment(models.Model):
     pass
 
-# Auction listings
+# ModelForms
 
-# Bids
 
-# Comments on auction listings
+CATEGORIES = [
+    ('apparel', 'Apparel'),
+    ('footwear', 'Footwear'),
+    ('home', 'Home'),
+    ('accessories', 'Accessories'),
+    ('sporting goods', 'Sporting Goods')
+]
+
+
+class CreateNewListing(ModelForm):
+    starting_bid = forms.DecimalField(
+        max_digits=10, decimal_places=2, min_value=0, initial=0.000)
+
+    class Meta:
+        model = AuctionListing
+        fields = ['listing_title', 'description', 'category',
+                  'starting_bid', 'image']
+        labels = {
+            'listing_title': 'title',
+            'description': 'description',
+            'category': 'category',
+            'starting_bid': 'starting_bid',
+            'image': 'image'
+        }
+        widgets = {
+            'listing_title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter your listing title.'}),
+            'description': Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter a descripton of your listing.', 'cols': 45, 'rows': 10}),
+            'category': forms.Select(choices=CATEGORIES),
+        }
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -1,32 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
+from django.forms.models import ModelForm
 from django.forms.widgets import NumberInput
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-from django import forms
 
-from .models import User, AuctionListing
-
-CATEGORIES = [
-    ('apparel', 'Apparel'),
-    ('footwear', 'Footwear'),
-    ('home', 'Home'),
-    ('accessories', 'Accessories'),
-    ('sporting goods', 'Sporting Goods')
-]
-
-
-class CreateNewListing(forms.Form):
-    listing_title = forms.CharField(required="true", label="listing_title", max_length=100,  widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Enter your listing title.'}))
-    description = forms.CharField(required="true", label="description", widget=forms.Textarea(
-        attrs={'class': 'form-control', 'placeholder': 'Enter a descripton of your listing.', 'cols': 45, 'rows': 10}))
-    category = forms.CharField(required="true",
-                               label="Product Category", max_length=25, widget=forms.Select(choices=CATEGORIES))
-    image = forms.ImageField(required="true", label="image")
-    price = forms.DecimalField(required="true",
-                               label="price", max_digits=10, decimal_places=2, min_value=0, widget=forms.NumberInput(attrs={'placeholder': '$0.00'}))
+from .models import User, CreateNewListing
 
 
 def index(request):
@@ -37,12 +17,9 @@ def new(request):
     if request.method == "POST":
         form = CreateNewListing(request.POST, request.FILES)
         if form.is_valid():
-            title = form.cleaned_data["listing_title"]
-            description = form.cleaned_data["description"]
-            category = form.cleaned_data["category"]
-            price = form.cleaned_data["price"]
-            image = form.cleaned_data["image"]
-            print(f"{title}, {description}, {category}, {price}")
+            price = form.cleaned_data["starting_bid"]
+            print(f"{price}")
+            form.save()
 
     return render(request, "auctions/new.html", {
         'form': CreateNewListing()
