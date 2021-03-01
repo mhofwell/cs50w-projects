f7910cfac3ca6401afe26494b1cac011e0b8b59e
Michael Hofweller <michael.hofweller@gmail.com>
2021-03-01 12:51:56 -0800

    fetch

--- w5/counter.js
+++ w5/counter.js
@@ -1,7 +1,12 @@
-let counter = 0;
+if (!localStorage.getItem('counter')) {
+        localStorage.setItem('counter', 0);
+}
+
 function count() {
+        let counter = localStorage.getItem('counter');
         counter++;
         document.querySelector('h1').innerHTML = counter;
+        localStorage.setItem('counter', counter);
 
         if (counter % 10 === 0) {
                 alert(`The counter is now ${counter}`);
@@ -9,5 +14,6 @@ function count() {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+        document.querySelector('h1').innerHTML = localStorage.getItem('counter');
         document.querySelector('button').onclick = count;
 });
