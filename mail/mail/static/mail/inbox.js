4f71750f8b9cc108dd5aaf9df09f4b241f6f9edc
Michael Hofweller <michael.hofweller@gmail.com>
2021-03-05 16:28:39 -0800

    email mailbox views complete

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,22 +1,15 @@
 // When the page loads, load its default state
 document.addEventListener('DOMContentLoaded', function() {
-        document.querySelectorAll('.btn-outline-primary').forEach(button => {
-                button.onclick = function() {
-                        const mailbox = this.id;
+        // Use buttons to toggle between views
+        document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+        document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+        document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+        document.querySelector('#compose').addEventListener('click', compose_email);
 
-                        // Add the current state to the history
-                        history.pushState({ mailbox }, '', `/emails/${mailbox}`);
-                        if (mailbox !== 'compose') {
-                                load_mailbox(mailbox);
-                        } else {
-                                compose_email();
-                        }
-                };
-        });
-
+        // event listener for button submit
         document.querySelector('#compose-form').onsubmit = send;
 
-        // Check to see if all the fields are filled before allowing a message to send.
+        // Check to see if all the compose form fields are filled before allowing a message to send.
         document.querySelector('#submit').disabled = true;
 
         const to = document.querySelector('#compose-recipients');
@@ -35,14 +28,64 @@ document.addEventListener('DOMContentLoaded', function() {
         load_mailbox('inbox');
 });
 
-window.onpopstate = function(event) {
-        console.log(event.state.mailbox);
-        if (event.state.mailbox !== 'compose') {
-                load_mailbox(event.state.mailbox);
-        } else {
-                compose_email();
-        }
-};
+function openEmail(id) {
+        // Show single email view and hide other views
+        document.querySelector('#emails-view').style.display = 'none';
+        document.querySelector('#compose-view').style.display = 'none';
+        const emailSingleView = document.querySelector('#email-single-view');
+        emailSingleView.style.display = 'block';
+
+        fetch(`/emails/${id}`)
+                .then(response => response.json())
+                .then(data => {
+                        JSON.stringify(data);
+                        getSingleEmail(data);
+                });
+}
+
+function getSingleEmail(data) {
+        // reset innerHTML of div so emails don't stack
+        document.querySelector('#email-single-view').innerHTML = '';
+
+        // fetch the required email data and create an element for it
+        const element = document.createElement('div');
+        element.innerHTML = `
+                <div class='email-container-singleview'>
+                        <div class='email-line'>
+                        <strong>${data.timestamp}</strong>
+                        </div>
+        
+                        <div class='email-line'>
+                        <strong>Subject:</strong> ${data.subject}
+                        </div>
+                        
+                        <div class='email-line'>
+                        <strong>From:</strong> ${data.sender}
+                        </div>
+        
+                        <div class='email-line'>
+                        <strong>To:</strong> ${data.recipients}
+                        </div>
+                        
+                        <hr>
+                        <div class='email-line'>
+                        ${data.body}
+                        </div>
+                        <hr>
+                        
+                        <div class='button-row'>
+                                <div class='email-button'>
+                                        <input class="btn btn-primary" type="submit" value="Reply"> 
+                                </div>
+                                <div class='email-button-reply'>
+                                        <input class="btn btn-primary" type="submit" value="Archive">    
+                                </div>         
+                        </div>
+                </div>
+                `;
+
+        document.querySelector('#email-single-view').append(element);
+}
 
 function compose_email() {
         // Show compose view and hide other views
@@ -59,6 +102,7 @@ function load_mailbox(mailbox) {
         // Show the mailbox and hide other views
         document.querySelector('#emails-view').style.display = 'block';
         document.querySelector('#compose-view').style.display = 'none';
+        document.querySelector('#email-single-view').style.display = 'none';
 
         // Show the mailbox name
         document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() +
@@ -78,41 +122,43 @@ function addEmail(email, mailbox) {
         // create new email
         const element = document.createElement('div');
         element.className = 'email';
-        console.log(email.read);
         // add content to email div
         if (mailbox === 'sent') {
                 element.innerHTML = `
-          <div class=email-heading-container>
-                <div class=email-line><strong> ${email.recipients}</strong></div>
-                <div class=email-line><strong></strong>${email.subject}</div> 
-          </div>
-          <div class=email-heading-container-date>      
-                <div class=email-date><strong></strong>${email.timestamp}</div>
-          </div>
+                <div class='email-heading-container'>
+                        <div class='email-line'><strong> ${email.recipients}</strong></div>
+                        <div class='email-line'><strong></strong>${email.subject}</div> 
+                </div>
+                <div class='email-heading-container-date'>      
+                        <div class='email-date'><strong></strong>${email.timestamp}</div>
+                </div>
           `;
         } else if (mailbox === 'inbox') {
                 element.innerHTML = `
-          <div class=email-heading-container>
-                <div class=email-line><strong>${email.sender}</strong></div>
-                <div class=email-line><strong></strong>${email.subject}</div> 
-          </div>
-          <div class=email-heading-container-date>      
-                <div class=email-date><strong></strong>${email.timestamp}</div>
-          </div>
+                <div class='email-heading-container'>
+                        <div class='email-line'><strong>${email.sender}</strong></div>
+                        <div class='email-line'><strong></strong>${email.subject}</div> 
+                </div>
+                <div class='email-heading-container-date'>      
+                        <div class='email-date'><strong></strong>${email.timestamp}</div>
+                </div>
           `;
         } else if (email.archived === true) {
                 element.innerHTML = `
-          <div class=email-heading-container>
-                <div class=email-line><strong>${email.sender}</strong></div>
-                <div class=email-line><strong></strong>${email.subject}</div> 
-          </div>
-          <div class=email-heading-container-date>      
-                <div class=email-date><strong></strong>${email.timestamp}</div>
-          </div>`;
+                <div class='email-heading-container'>
+                        <div class='email-line'><strong>${email.sender}</strong></div>
+                        <div class='email-line'><strong></strong>${email.subject}</div> 
+                </div>
+                <div class='email-heading-container-date'>      
+                        <div class='email-date'><strong></strong>${email.timestamp}</div>
+                </div>`;
         }
 
+        element.addEventListener('click', () => {
+                openEmail(email.id);
+        });
+
         if (email.read === true) {
-                console.log(email.read);
                 element.classList.add('read');
         }
 
@@ -131,6 +177,6 @@ function send() {
                 .then(response => response.json())
                 .then(result => {
                         console.log(result);
-                        load_mailbox('inbox');
                 });
+        load_mailbox('sent');
 }
