54027398e8013e149aaeb5abef5a044686198316
Michael Hofweller <michael.hofweller@gmail.com>
2021-04-04 18:00:06 -0700

    good

---
+++ project4/network/urls.py
@@ -0,0 +1,21 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("profile/<str:username>", views.get_profile, name="profile"),
+
+
+
+    # API Routes
+    path("post", views.post, name="post"),
+    path("posts/<str:group>", views.load_posts, name="posts"),
+    path("follow/<str:username>", views.follow, name="follow"),
+    path("followcount/<str:username>", views.count, name="count"),
+    # path("unfollow/<str:username>", views.unfollow, name="unfollow"),
+]
